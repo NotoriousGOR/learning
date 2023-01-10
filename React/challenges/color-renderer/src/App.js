d59cc96fed485f7084a827c2c675a7299c8a6258
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 12:00:28 -0600

    Finished challenge 1

---
+++ React/challenges/color-renderer/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ColorRenderer from './components/ColorRender';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ColorRenderer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/challenges/color-renderer/src/components/Color.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Color({hex, name}) {
+  return (
+     <div
+      className='color-square' style={{ backgroundColor: hex }}
+    >
+      <h2>{name}</h2>
+    </div>
+  )
+}
