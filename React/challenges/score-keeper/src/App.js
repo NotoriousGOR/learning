c8c9569c6117a357c1430e2c31c4b6ae8459ec0f
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 14:11:03 -0600

    solved scoreKeeper challenge

---
+++ React/challenges/score-keeper/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ScoreKeeper from './components/ScoreKeeper';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ScoreKeeper/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
