6785ad4eeded2657a62ebef982612f16c223eed6
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 13:12:12 -0600

    changed to a better method of solving this problem

--- React/challenges/dark-mode/src/components/DarkMode.jsx
+++ React/challenges/dark-mode/src/components/DarkMode.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 export default function DarkMode() {
+    const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className="page">
-      <button onClick={() => {
-        document.body.classList.add("dark-mode");
-      }}  className="dark-mode-button">Dark Mode</button>
-      <button onClick={() => {
-        document.body.classList.remove("dark-mode");
-      }} className="light-mode-button">Light Mode</button>
+    <div className={`page ${darkMode && "dark-mode"}`}>
+      <button onClick={() => setDarkMode(true)}  className="dark-mode-button">Dark Mode</button>
+      <button onClick={() => setDarkMode(false)} className="light-mode-button">Light Mode</button>
     </div>
   );
 }
