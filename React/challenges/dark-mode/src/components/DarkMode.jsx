6462ccd0a91b6e706a4f70823cc04e773f7e30b5
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 13:06:07 -0600

    working on challenge 2

---
+++ React/challenges/dark-mode/src/components/DarkMode.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DarkMode() {
+  return (
+    <div className="page">
+      <button className="dark-mode-button">Dark Mode</button>
+      <button className="light-mode-button">Light Mode</button>
+    </div>
+  );
+}
