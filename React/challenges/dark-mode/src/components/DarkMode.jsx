775be300a89636454ff563601c6a7cc19e203323
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 13:08:27 -0600

    added dark more toggle

--- React/challenges/dark-mode/src/components/DarkMode.jsx
+++ React/challenges/dark-mode/src/components/DarkMode.jsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function DarkMode() {
   return (
     <div className="page">
-      <button className="dark-mode-button">Dark Mode</button>
-      <button className="light-mode-button">Light Mode</button>
+      <button onClick={() => {
+        document.body.classList.add("dark-mode");
+      }}  className="dark-mode-button">Dark Mode</button>
+      <button onClick={() => {
+        document.body.classList.remove("dark-mode");
+      }} className="light-mode-button">Light Mode</button>
     </div>
   );
 }
