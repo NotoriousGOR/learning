8fdf2c013b3c23169a003dabbbe9a41eaea81a28
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 15:00:47 -0600

    finished color picker challenge

---
+++ React/challenges/color-picker/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorPicker from './components/ColorPicker';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ColorPicker/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/challenges/color-picker/src/components/Color.jsx
@@ -0,0 +1,11 @@
+export default function Color ({ hex, name, changeColor }) {
+    return (
+      <button
+        className='color-square'
+        style={{ backgroundColor: hex }}
+        onClick={() => changeColor(hex)}
+      >
+        <h2>{name}</h2>
+      </button>
+    )
+  }
