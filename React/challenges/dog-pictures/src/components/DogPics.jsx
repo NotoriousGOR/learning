d84b160d317a5f36521e378ca8c5835ac67f13d3
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-10 14:00:26 -0600

    finished challenge

---
+++ React/challenges/dog-pictures/src/components/DogPics.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+export default function DogPics() {
+  const [dogsPic, setDogPic] = useState("https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg");
+
+  /// fetch from: https://dog.ceo/api/breeds/image/random Fetch!
+
+  const fetchDoggyPics = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((data) => data.json())
+      .then((data) => {
+        setDogPic(data.message)
+      });
+  };
+
+  return (
+    <div className="dog-pics">
+      <img src={dogsPic} alt="dog pic" />
+      <button onClick={fetchDoggyPics}>🐶</button>
+    </div>
+  );
+}
