02769d96605ba8eb0a09471d1abf7d877bfb4895
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-11 19:36:44 -0600

    added getServerSideProps for dynamic content

--- Next.js/learning/pages/news/[query].jsx
+++ Next.js/learning/pages/news/[query].jsx
@@ -2,14 +2,14 @@ import Layout from '../../components/Layout'
 import { Inter } from '@next/font/google'
 import styles from '../../styles/Home.module.css'
 import Link from 'next/link'
-import { handler } from '../api'
+import { search } from '../api'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function News({ results }) {
+export default function NewsQuery({ results }) {
   return (
     <Layout className="flex align-middle justify-center">
-      <h1 className={inter.className}>Top Stories</h1>
+      <h1 className={inter.className}>Search</h1>
       <ul>
         {results.map((result) => (
           <li className="m-2" key={result.uri}>
@@ -29,13 +29,12 @@ export default function News({ results }) {
 
 const API_KEY = '9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ'
 
-export async function getStaticProps() {
-  const url = `https://api.nytimes.com/svc/topstories/v2/us.json?api-key=${API_KEY}`
-  const response = await handler(url)
+export async function getServerSideProps({ params }) {
+  const results = await search(
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${params.query}&api-key=${API_KEY}`,
+  )
 
   return {
-    props: {
-      results: response,
-    },
+    props: { results },
   }
 }
