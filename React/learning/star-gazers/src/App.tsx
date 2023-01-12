71c318e0cda4480b2a992dfd6978696d9c034be8
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-12 09:20:43 -0600

    added typescript and event handler type

--- React/learning/star-gazers/src/App.tsx
+++ React/learning/star-gazers/src/App.tsx
@@ -1,58 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
-
-type InputData = {
-  first_name: string
-  last_name: string
-  age: number
-}
+import Form from './components/Form'
 
 export default function App() {
-  const [input, setInput] = useState({} as InputData)
-
-  const handleChange = (event) => {
-    const { name, value } = event.target
-    setInput((values) => ({ ...values, [name]: value }))
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    alert(
-      `hello, ${input.first_name} ${input.last_name}. Are you ${input.age} years old?`,
-    )
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter your first name:
-        <input
-          type="text"
-          name="first_name"
-          value={input.first_name || ''}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter your last name:
-        <input
-          type="text"
-          name="last_name"
-          value={input.last_name || ''}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter your age:
-        <input
-          type="number"
-          name="age"
-          value={input.age || ''}
-          onChange={handleChange}
-        />
-      </label>
-      <input type="submit" />
-    </form>
+  return(
+    <div>
+      <Form/>
+    </div>
   )
 }
