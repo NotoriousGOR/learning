d2c56274e0cf0ba26b86b1bb269729c61cf35c9e
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2023-01-13 12:52:57 -0600

    working on things

--- Next.js/learning/pages/api/contacts.js
+++ Next.js/learning/pages/api/contacts.js
@@ -1,32 +1,37 @@
-export default [
-  {
-    id: 'alfreds-futterkiste',
-    name: 'Alfreds Futterkiste',
-    country: 'Germany',
-  },
-  {
-    id: 'francisco-chang',
-    name: 'Francisco Chang',
-    country: 'Mexico',
-  },
-  {
-    id: 'ernst-handel',
-    name: 'Ernst Handel',
-    country: 'Austria',
-  },
-  {
-    id: 'helen-bennett',
-    name: 'Helen Bennett',
-    country: 'UK',
-  },
-  {
-    id: 'yoshi-tannamuri',
-    name: 'Yoshi Tannamuri',
-    country: 'Canada',
-  },
-  {
-    id: 'giovanni-rovelli',
-    name: 'Giovanni Rovelli',
-    country: 'Italy',
-  },
-]
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      id: 'alfreds-futterkiste',
+      name: 'Alfreds Futterkiste',
+      country: 'Germany',
+    },
+    {
+      id: 'francisco-chang',
+      name: 'Francisco Chang',
+      country: 'Mexico',
+    },
+    {
+      id: 'ernst-handel',
+      name: 'Ernst Handel',
+      country: 'Austria',
+    },
+    {
+      id: 'helen-bennett',
+      name: 'Helen Bennett',
+      country: 'UK',
+    },
+    {
+      id: 'yoshi-tannamuri',
+      name: 'Yoshi Tannamuri',
+      country: 'Canada',
+    },
+    {
+      id: 'giovanni-rovelli',
+      name: 'Giovanni Rovelli',
+      country: 'Italy',
+    },
+  ]
+  )
+}
--- React/learning/star-gazers/src/App.tsx
+++ React/learning/star-gazers/src/App.tsx
@@ -1,20 +1,66 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Form from './components/Form'
+import Table from './components/Table'
 
 export default function App() {
   const [shows, setShows] = useState()
 
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: 'TV Show',
+        // First group columns
+        columns: [
+          {
+            Header: 'Name',
+            accessor: 'show.name',
+          },
+          {
+            Header: 'Type',
+            accessor: 'show.type',
+          },
+        ],
+      },
+      {
+        // Second group - Details
+        Header: 'Details',
+        // Second group columns
+        columns: [
+          {
+            Header: 'Language',
+            accessor: 'show.language',
+          },
+          {
+            Header: 'Genre(s)',
+            accessor: 'show.genres',
+          },
+          {
+            Header: 'Runtime',
+            accessor: 'show.runtime',
+          },
+          {
+            Header: 'Status',
+            accessor: 'show.status',
+          },
+        ],
+      },
+    ],
+    [],
+  )
+
   useEffect(() => {
-    (async () => {
-      const result = await fetch('https://api.tvmaze.com/search/shows?q=snow')
-      const data = await result.json();
-    })()
+    fetch('https://api.tvmaze.com/search/shows?q=snow')
+      .then((res) => res.json())
+      .then((data) => setShows(data))
   }, [])
 
   return (
     <div>
       <Form />
+      <Table columns={columns} data={shows} />
     </div>
   )
 }
