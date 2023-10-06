a013380bfeab01a1bc5b083d36db3c3cd7936a85
Gabriel Rosales <gabriel@coldsmoke.co>
2023-10-06 10:13:31 -0500

    Work

--- Next.js/learning/pages/404.jsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout'
-
-export default function ErrorPage() {
-  return (
-    <Layout>
-      <h2>Not found</h2>
-    </Layout>
-  )
-}
--- Next.js/learning/pages/learn/next.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function LearnNext() {
-  return (
-    <div>Learn Next.js</div>
-  )
-}
