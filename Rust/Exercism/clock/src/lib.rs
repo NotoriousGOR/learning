440358d3c993a3d05ca3dc0404955bc34def85c2
Gabriel Rosales <Gabriel.rosales0129@gmail.com>
2025-09-23 22:27:45 -0500

    finished test

--- Rust/Exercism/clock/src/lib.rs
+++ Rust/Exercism/clock/src/lib.rs
@@ -1,11 +1,38 @@
-pub struct Clock;
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        if minutes > 59 || minutes < 0 {
+            let extra_hours = minutes.div_euclid(60);
+            let new_minutes = minutes.rem_euclid(60);
+            
+            return Clock::new(hours + extra_hours, new_minutes);
+        }
+
+        if hours > 23 || hours < 0 {
+            let new_hours = hours.rem_euclid(24);
+            return Clock::new(new_hours, minutes);
+        }
+
+        let hours = hours.rem_euclid(24);
+        let minutes = minutes.rem_euclid(60);
+
+        return Clock { hours, minutes };
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        return Clock::new(self.hours, self.minutes + minutes);
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
